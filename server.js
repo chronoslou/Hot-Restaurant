dea9c72b832938270747b37991c28f267619ce44
Taylor Leong <tjl2125@alum.barnard.edu>
2021-04-10 14:02:26 -0400

    added app.post and added fetch to tables.html

--- server.js
+++ server.js
@@ -3,18 +3,12 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const tables = require ('./data/tableData');
+const waitlist = require ('./data/waitinglistData'); 
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-const reservations = [
-
-]
-
-const waitlist = [
-    
-]
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/home.html')));
 
@@ -22,9 +16,23 @@ app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'public/rese
 
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'public/tables.html')));
 
-app.get("/api/reservations", (req, res) => res.json(reservations) );
+app.get("/api/tables", (req, res) => res.json(tables) );
 
 app.get("/api/waitlist", (req, res) => res.json(waitlist) );
 
+app.post('/api/tables', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    if (tables.length < 5){
+    const newTables = req.body;
+
+    console.log(newTables);
+  
+    tables.push(newTables);
+    res.json(newTables);
+    } else {
+        waitlist.push(newTables);
+    }
+  });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
